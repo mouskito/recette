179d6c702fb93109bf6bfcd39de76ed1981ae66f
Moussa CAMARA <msa.camara@gmail.com>
2023-11-24 17:59:26 +0100

    step1-ajout-page-accueil

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    require "header.php";
+?>
+
+<div class="container">
+    <h3>Partage tes recettes</h3>
+    <p>
+        Une alimentation saine et équilibrée est essentielle pour le bon fonctionnement de notre corps. Elle permet de maintenir un poids stable, de renforcer notre système immunitaire, de prévenir de nombreuses maladies chroniques et de nous donner de l’énergie pour affronter la journée. De plus, manger des aliments sains peut également améliorer notre humeur et notre bien-être général.
+    </p>
+
+    
+</div>
